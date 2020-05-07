956d18b593779e267c1dd8c477a38903934ee2d9
Ivan <45362427+Efcolipt@users.noreply.github.com>
2020-05-07 21:32:14 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,20 +1,20 @@
 <?php 
 
 
-	$host = "localhost";
-	$user = "u0800927_default";
-	$pass = "IKo2_5Wt";
+	$host = "";
+	$user = "";
+	$pass = "";
 	$datatableContact = "Contact";
 	$datatablePerson = "Users";
 	$datatablePortfolio = "Portfolio";
 	$datatableTestimonial = "Testimonials";
-	$database = "u0800927_default";
+	$database = "";
 
 	$link = mysqli_connect($host, $user, $pass, $database); 
 	if (mysqli_connect_errno( $link )) { echo "Failed to connect to MySQL: " . mysqli_connect_error(); }
 	
 	mysqli_query($link,"SET NAMES 'utf8'"); 
-    mysqli_query($link,"SET CHARACTER SET 'utf8'");
-    mysqli_query($link,"SET SESSION collation_connection = 'utf8_general_ci'");
+   	mysqli_query($link,"SET CHARACTER SET 'utf8'");
+   	mysqli_query($link,"SET SESSION collation_connection = 'utf8_general_ci'");
 
 ?>
